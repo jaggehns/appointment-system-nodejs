b3aa0b1f8d0b536d2dca5d2655536f3d7eb7e3d5
Jaggehn <jaggehns@gmail.com>
2024-11-16 19:14:59 +0800

    feat: integrated config checking in appointment api

--- src/service/appointment.service.ts
+++ src/service/appointment.service.ts
@@ -1,4 +1,6 @@
 import { appointmentModel } from '../models/appointment.model'
+import { configurationModel } from '../models/configuration.model'
+import { type UpdateConfigurationInput } from '../schema/configuration.schema'
 
 interface AvailableSlot {
   time: string
@@ -6,16 +8,18 @@ interface AvailableSlot {
 }
 
 const getAvailableSlots = async (date: Date): Promise<AvailableSlot[]> => {
-  const config = await appointmentModel.getConfiguration()
-  if (config == null) throw new Error('Configuration not found')
+  const config: UpdateConfigurationInput['body'] | null = await configurationModel.getFullConfiguration()
 
-  const startHour = parseInt(config.startHour.split(':')[0])
-  const endHour = parseInt(config.endHour.split(':')[0])
+  const startHour =
+    config?.startHour != null && config.startHour.trim() !== '' ? parseInt(config.startHour.split(':')[0]) : 9
+  const endHour = config?.endHour != null && config.endHour.trim() !== '' ? parseInt(config.endHour.split(':')[0]) : 18
+  const maxSlots = config?.maxSlots ?? 1
+
   const slots: AvailableSlot[] = []
 
   for (let hour = startHour; hour < endHour; hour++) {
-    slots.push({ time: `${hour}:00`, available_slots: config.maxSlots })
-    slots.push({ time: `${hour}:30`, available_slots: config.maxSlots })
+    slots.push({ time: `${hour}:00`, available_slots: maxSlots })
+    slots.push({ time: `${hour}:30`, available_slots: maxSlots })
   }
 
   const appointments = await appointmentModel.getAvailableAppointments(date)
@@ -31,10 +35,14 @@ const getAvailableSlots = async (date: Date): Promise<AvailableSlot[]> => {
 }
 
 const bookAppointment = async (date: Date, time: string, slots: number): Promise<boolean> => {
+  const config: UpdateConfigurationInput['body'] | null = await configurationModel.getFullConfiguration()
+
+  const maxSlots = config?.maxSlots ?? 1
+
   const existingAppointments = await appointmentModel.getAvailableAppointments(date)
   const existingAppointment = existingAppointments.find((appt) => appt.time.toTimeString().slice(0, 5) === time)
 
-  if (existingAppointment != null && existingAppointment.slotsBooked >= slots) {
+  if (existingAppointment != null && existingAppointment.slotsBooked >= maxSlots) {
     return false
   }
 
